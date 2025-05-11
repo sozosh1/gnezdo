4443a9cd722e48c8a9b132e710696a0957705652
sozosh1 <arbaevaa@mail.ru>
2025-05-12 02:19:08 +0300

    add auth_bloc 0.0.2

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gnezdo/app_screen.dart';
-import 'package:gnezdo/features/auth/bloc/authentication_bloc.dart';
+import 'package:gnezdo/features/auth/bloc/auth_bloc/authentication_bloc.dart';
 import 'package:user_repository/user_repository.dart';
 
 class MyApp extends StatelessWidget {
